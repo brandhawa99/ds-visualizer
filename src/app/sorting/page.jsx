af392959329eeda203e04232f929542fc9c70f4b
brandhawa99 <baltej.randhawa0@gmail.com>
2024-07-24 10:43:33 -0700

    created sorting page

---
+++ src/app/sorting/page.jsx
@@ -0,0 +1,14 @@
+import {Main, Container,Section} from "@/components/craft"
+import BubbleSort from "@/components/sorting/bubblesort"
+export default function Sorting (){
+  return(
+    <Main>
+      <Section>
+      <Container>
+        <BubbleSort />
+        
+      </Container>
+      </Section>
+    </Main>
+  ) 
+}

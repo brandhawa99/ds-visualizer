21e978d188a7adc44b9fbcf76b4279859469336c
brandhawa99 <baltej.randhawa0@gmail.com>
2024-07-23 21:28:06 -0700

    added footer and features to home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,20 @@
-import styles from "./page.module.css";
 import {Container, Main, Section} from "@/components/craft"
 import Hero from "@/components/home-page/hero"
-
+import FeatureLeft from "@/components/home-page/feature-left";
+import FeatureRight from "@/components/home-page/feature-right";
+import Footer from "@/components/home-page/footer";
 export default function Home() {
   return (
   <Main>
     <Section>
-    <Container>
-      <Hero /> 
-    </Container>
+      <Container>
+        <Hero /> 
+        <FeatureLeft />
+        <FeatureRight />
+        <Footer />
+      </Container>
     </Section>
+
   </Main>
   );
 }

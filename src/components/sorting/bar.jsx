639534cca93c78941236d92660880cb66e67bda6
brandhawa99 <baltej.randhawa0@gmail.com>
2024-07-24 18:42:04 -0700

    created bubble sort component

---
+++ src/components/sorting/bar.jsx
@@ -0,0 +1,7 @@
+export default function Bar (){
+  return(
+    <div>
+      hello
+    </div>
+  )
+}
---
+++ src/components/sorting/bubblesort.jsx
@@ -0,0 +1,15 @@
+import {Container, Section } from "@/components/craft"
+import Bar from "@/components/sorting/bar"
+export default function BubbleSort() {
+
+  return(
+    <Section>
+      <Container>
+        <h1>Bubble Sort</h1>
+        <div>
+          <Bar />
+        </div>
+      </Container>
+    </Section>
+  )
+}
